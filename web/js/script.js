3947edfcfaa20a9db4d26727fb3df98b8cba7865
vilsonfcastilho <vilsonfcastilho@gmail.com>
2019-10-20 21:43:18 -0300

    Inserindo Js

---
+++ web/js/script.js
@@ -0,0 +1,17 @@
+if(window.SimpleSlide) {
+
+new SimpleSlide({
+	slide: "quote", // nome do atributo data-slide="principal"
+	time: 5000 // tempo de transição dos slides
+});
+
+new SimpleSlide({
+	slide: "portfolio", // nome do atributo data-slide="principal"
+	time: 5000 // tempo de transição dos slides
+});
+
+}
+
+if(window.SimpleAnime) {
+new SimpleAnime();
+}
