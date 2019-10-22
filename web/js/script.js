adf542de3377eaac0826e012171c3a9017289b8e
vilsonfcastilho <vilsonfcastilho@gmail.com>
2019-10-22 01:07:57 -0300

    Inserindo Form

--- web/js/script.js
+++ web/js/script.js
@@ -1,17 +1,31 @@
 if(window.SimpleSlide) {
 
-new SimpleSlide({
-	slide: "quote", // nome do atributo data-slide="principal"
-	time: 5000 // tempo de transição dos slides
-});
+	new SimpleSlide({
+		slide: "quote", // nome do atributo data-slide="principal"
+		time: 5000 // tempo de transição dos slides
+	});
 
-new SimpleSlide({
-	slide: "portfolio", // nome do atributo data-slide="principal"
-	time: 5000 // tempo de transição dos slides
-});
+	new SimpleSlide({
+		slide: "portfolio",
+		time: 5000,
+		nav: true
+	});
 
 }
 
 if(window.SimpleAnime) {
-new SimpleAnime();
+
+	new SimpleAnime();
+	
+}
+
+if(window.SimpleForm) {
+
+	new SimpleForm({
+		form: ".formphp",
+		button: "#enviar",
+		erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com</p></div>",
+		sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso!</h2><p>Em breve entro em contato com você.</p></div>"
+	});
+
 }
